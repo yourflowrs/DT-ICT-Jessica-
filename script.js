6e6247b4b32fb240de0ce08dae0188ef4a61f01b
yourflowrs <violietaflow@gmail.com>
2025-05-20 10:41:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-/*Dikerjakan oleh Gaby*/
-
-
-//Memastikan hanya bagian yang termasuk pada id 'Hacking' yang terlihat
-function open1() {
-	document.getElementById("UU").style.display = "none"; //Menghilangkan bagian dengan id 'UU'
-	document.getElementById("Hacking").style.display = "block"; //Memunculkan bagian dengan id 'Hacking'
-	document.getElementById("Kasus").style.display = "none"; //Menghilangkan bagian dengan id 'Kasus'
-
-}
-
-//Memastikan hanya bagian yang termasuk pada id 'UU' yang terlihat
-function open2() {
-	document.getElementById("UU").style.display = "block"; //Memunculkan bagian dengan id 'UU'
-	document.getElementById("Hacking").style.display = "none"; //Menghilangkan bagian dengan id 'Hacking'
-	document.getElementById("Kasus").style.display = "none";  //Menghilangkan bagian dengan id 'Kasus'
-}
-
-//Memastikan hanya bagian yang termasuk pada id 'Kasus' yang terlihat
-function open3() {
-	document.getElementById("UU").style.display = "none"; //Menghilangkan bagian dengan id 'UU'
-	document.getElementById("Hacking").style.display = "none"; //Menghilangkan bagian dengan id 'Hacking'
-	document.getElementById("Kasus").style.display = "block"; //Memunculkan bagian dengan id 'Kasus'
-}
-
+//Punya gaby
 // Menetapkan variabel dark
 let dark = localStorage.getItem('dark')
 // Menentukan variabel darkmode dengan tombol dengan id ‘darkmode’
@@ -30,14 +6,14 @@ const darkmode = document.getElementById('darkmode')
 
 // Menentukan fungsi enabledark yang menetapkan variabel dark sebagai aktif
 const enabledark = () => {
-	document.body.classList.add('dark')
-	localStorage.setItem('dark','active')
+  document.body.classList.add('dark')
+  localStorage.setItem('dark','active')
 }
 
 // Menentukan fungsi disable yang menetapkan variabel dark sebagai null dan tidak aktif
 const disabledark = () => {
-	document.body.classList.remove('dark')
-	localStorage.setItem('dark', null)
+  document.body.classList.remove('dark')
+  localStorage.setItem('dark', null)
 }
 
 // Menentukan bahwa jika dark aktif, menjalankan fungsi enabledark
@@ -47,10 +23,10 @@ if (dark === "active") enabledark()
 /*Kalau dark saat di local storage memiliki value yang tidak sama dengan aktif, maka menjalankan fungsi enable dark. 
 Namun, jika value sama dengan aktif, maka menjalankan fungsi disable dark.*/
 darkmode.addEventListener("click", () => {
-	dark = localStorage.getItem('dark')
-	if (dark !== "active"){
-		enabledark()
-	} else{
-		disabledark()
-	}
+  dark = localStorage.getItem('dark')
+  if (dark !== "active"){
+    enabledark()
+  } else{
+    disabledark()
+  }
 })
